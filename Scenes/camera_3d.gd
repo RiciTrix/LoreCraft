410ae04a14c8c5f621129ff88c30e059bc88d639
RiciTrix <richardgroenewald5@gmail.com>
2024-11-06 19:20:30 +0200

    Final Prototype
    Added new environment prefabs
    Fixed Camera jitter
    Increased Player Height
    Baked new lightmaps
    Created new test level with slopes included

--- Scenes/camera_3d.gd
+++ Scenes/camera_3d.gd
@@ -4,6 +4,7 @@ extends Camera3D
 @export var rootNode: CharacterBody3D
 @export var mainNode: Node3D
 @export var pivotPoint: Node3D
+var followPos
 
 @export_group("Settings")
 @export var sensitivity: float
@@ -12,17 +13,19 @@ extends Camera3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	followPos = rootNode.get_child(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		
+		mainNode.position = followPos.global_position
 		if Input.is_action_just_pressed("esc"):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		rootNode.rotation.y += deg_to_rad(event.relative.x) * -sensitivity
+		mainNode.rotation.y = rootNode.rotation.y
 		var xRotation = mainNode.rotation.x + deg_to_rad(event.relative.y) * -sensitivity
 		xRotation = clamp(xRotation, deg_to_rad(-90), deg_to_rad(90))
 		mainNode.rotation.x = xRotation
