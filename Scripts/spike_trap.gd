10b685e03a84cc1d08cf3c8ea2fff80671e212ac
RiciTrix <richardgroenewald5@gmail.com>
2024-11-16 23:14:45 +0200

    Finished Spike Trap

---
+++ Scripts/spike_trap.gd
@@ -0,0 +1,41 @@
+extends Trap
+@export var length: float
+@export var activeTime: float
+@export var activeTimer: Timer
+@export var speed: float
+
+@onready var spikes = $"Spike Trap Base/Spike Trap Spikes2"
+
+var originalPos
+var tween
+func activate():
+	super.activate()
+	activeTimer.wait_time = activeTime
+	originalPos = spikes.position
+	tween = create_tween()
+	tween.tween_property(spikes, "position", Vector3(0, originalPos.y + length, 0), speed)
+	tween.connect("finished", deactivate)
+
+var tween2
+func deactivate():
+	activeTimer.start()
+	tween.kill()
+	await activeTimer.timeout
+	tween = get_tree().create_tween()
+	tween.tween_property(spikes, "position", Vector3(0, originalPos.y, 0), speed)
+	if trapType == Types.AlwaysOn:
+		tween.connect("finished", reset)
+	else:
+		tween.connect("finished", disable)
+
+func disable():
+	super.deactivate()
+
+func reset():
+	tween.kill()
+	activeTimer.start()
+	await activeTimer.timeout
+	activate()
+
+func doTrapThings(delta):
+	pass
---
+++ Scripts/spinning_spike.gd
@@ -0,0 +1,14 @@
+extends Trap
+@export var spin_speed: float
+
+@export var spin_direction: Vector3i
+
+@onready var spike = $"Spinning Spike"
+
+func doTrapThings(delta):
+	spike.rotation += spin_speed * delta * spin_direction
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		print("Dead")
