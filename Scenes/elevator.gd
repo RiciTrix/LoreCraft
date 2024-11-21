ebdffd75792358db4a207e197bbe51dcc6041d1a
RiciTrix <richardgroenewald5@gmail.com>
2024-11-21 17:30:27 +0200

    Added New Prefabs
    Added Elevator
    Added Obelisk Prefab

---
+++ Scenes/elevator.gd
@@ -0,0 +1,22 @@
+extends Trap
+var tween
+
+@export var distance: float
+@export var direction: Vector3i
+@export var duration: float
+@export var enableReturn: bool
+
+func activate():
+	super.activate()
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "position", self.position + (distance * direction), duration)
+	tween.connect("finished", deactivate)
+	
+	
+func deactivate():
+	super.deactivate()
+	if enableReturn:
+		direction *= -1
+	
+func doTrapThings(delta):
+	pass
