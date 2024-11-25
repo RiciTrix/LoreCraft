2167be417f2429b31957b412a2d9596bdd00caf1
RiciTrix <richardgroenewald5@gmail.com>
2024-11-25 16:44:24 +0200

    Added Pressure Plate toggle

--- Scripts/pressure_plate.gd
+++ Scripts/pressure_plate.gd
@@ -1,11 +1,18 @@
 extends Node3D
 @export var whatToActivate: Array[Trap]
+@export var disableAfterFirstActivation: bool
+
+@onready var isActivated: bool = false
 
 func activate():
 	for trap in whatToActivate:
 		if !trap.is_active():
 			trap.activate()
+	isActivated = !isActivated
 
 
 func _on_area_3d_body_entered(body):
-	activate()
+	if isActivated && disableAfterFirstActivation:
+		return
+	else:
+		activate()
